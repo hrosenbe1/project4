40b228a52305b91d0e9d0de63755ca8dff73ade9
Heather Rosenberg <h.rosenberg79@gmail.com>
2016-06-02 22:40:01 -0400

    Added some bower components.

---
+++ client/app/app.constant.js
@@ -0,0 +1,13 @@
+(function(angular, undefined) {
+  angular.module("project4App.constants", [])
+
+.constant("appConfig", {
+	"userRoles": [
+		"guest",
+		"user",
+		"admin"
+	]
+})
+
+;
+})(angular);
--- client/app/app.js
+++ client/app/app.js
@@ -1,11 +1,19 @@
 'use strict';
 
-angular.module('project4App', ['project4App.auth', 'project4App.admin', 'project4App.constants',
-    'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'ui.bootstrap',
-    'validation.match'
+angular.module('heatherArtShopApp', [
+                 'heatherArtShopApp.auth',
+                 'heatherArtShopApp.admin',
+                 'heatherArtShopApp.constants',
+                 'ngCookies',
+                 'ngResource',
+                 'ngSanitize',
+                 'btford.socket-io',
+                 'ui.router',
+                 'ui.bootstrap',
+                 'validation.match',
+                 'ngAnimate'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
-
     $locationProvider.html5Mode(true);
   });
